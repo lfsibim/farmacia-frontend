48153ab4171871e531f251feaf577db2f0aa2b84
Luis Sibim <141850109+lfsibim@users.noreply.github.com>
2024-01-31 15:19:19 -0300

    adicionado CRUD de categoria

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
-import Home from './pages/home/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import FormularioCategoria from './components/categorias/formularioCategoria/FormularioCategoria'
+import DeletarCategoria from './components/categorias/deletarCategorias/DeletarCategoria'
+import ListaCategorias from './components/categorias/listaCategorias/ListaCategorias'
+
 
 function App() {
-
-  return (
-    <>
-      <BrowserRouter>
-      <Navbar />
-        <div className='min-h-[80vh]'>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-          </Routes>
-        </div>
-      <Footer />
-      </BrowserRouter>
-    </>
-  )
+    return (
+            <>
+            <BrowserRouter>
+                <Navbar />
+                <div className='min-h-[80vh]'>  
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/Categorias" element={<ListaCategorias />} />
+                        <Route path="/cadastroCategoria" element={<FormularioCategoria />} /> 
+                        <Route path="/editarCategoria/:id" element={<FormularioCategoria />} />
+                        <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
+                    </Routes>
+                </div>
+                <Footer />
+            </BrowserRouter>
+            </>
+    )
 }
 
 export default App
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,16 @@
+import { Link, useNavigate } from 'react-router-dom'
+
 function Navbar() {
  
   return (
     <>
      <div className='w-full bg-yellow-500 text-white flex justify-center py-4'>
           <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase text-gray-800'>Farmagen</div>
+            <Link to='/home' className='text-2xl font-bold uppercase text-gray-800'>Farmagen</Link>
 
             <div className='flex gap-4 text-gray-800'>
-              <div className='hover:text-zinc-950'>Produtos</div>
-              <div className='hover:underline'>Categorias</div>
-              <div className='hover:underline'>Cadastrar Categoria</div>
-              <div className='hover:underline'>Carrinho</div>
+            <Link to='/categorias' className='hover:underline'>Categorias</Link>
+            <Link to='/cadastroCategoria' className='hover:underline'>Cadastrar Categoria</Link>
             </div>
           </div>
         </div>
